654fca8f93ba58addeaa2a92934cf9d50b05a7e2
Claas Diederichs <claas.diederichs@uni-oldenburg.de>
2013-06-11 14:15:32 +0200

    removed invalid characters (old BOM)

--- modules/automation/projects/hilec/python/amir/Vector3D.py
+++ modules/automation/projects/hilec/python/amir/Vector3D.py
@@ -14,5 +14,5 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http:#www.gnu.org/licenses/>.
 
-﻿from offis.types import Vector3d as Vector3D;
+from offis.types import Vector3d as Vector3D;
 from offis.types import Vector3d;
--- modules/automation/projects/hilec/python/amir/rc.py
+++ modules/automation/projects/hilec/python/amir/rc.py
@@ -14,4 +14,4 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http:#www.gnu.org/licenses/>.
 
-﻿from offis.rc import *;
+from offis.rc import *;

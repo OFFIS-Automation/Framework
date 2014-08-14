1dd029e9441401601ab44e6cc600280c96ed74f9
Tobias T <tobias.tiemerding@uni-oldenburg.de>
2014-08-14 11:38:19 +0200

    Removed typo #11

--- modules/version/version.h
+++ modules/version/version.h
@@ -5,8 +5,8 @@ namespace Version
 {
 	static const int MAJOR = 1;
 	static const int MINOR = 0;
-	static const int BUILD = 14225;
-	static const QString DATE = "08/13/14";
+	static const int BUILD = 14226;
+	static const QString DATE = "08/14/14";
 }
 
 #endif // VERSION_H
